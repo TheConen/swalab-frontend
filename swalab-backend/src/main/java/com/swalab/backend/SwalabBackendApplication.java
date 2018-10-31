cccfd92d782426a2579c9813df1eb6b31b3b6620
DESKTOP-AHD9183\RebeccaS <rebecca.sigmund@web.de>
2018-10-31 13:10:43 +0100

    Created mock Database

--- swalab-backend/src/main/java/com/swalab/backend/SwalabBackendApplication.java
+++ swalab-backend/src/main/java/com/swalab/backend/SwalabBackendApplication.java
@@ -1,12 +1,19 @@
 package com.swalab.backend;
 
+import com.swalab.backend.database.Database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SwalabBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwalabBackendApplication.class, args);
+	}
+
+	@Bean
+	public Database database() {
+		return new Database();
 	}
 }
