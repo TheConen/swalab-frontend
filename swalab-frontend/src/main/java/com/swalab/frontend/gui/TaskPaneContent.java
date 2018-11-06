95a1c31ebac7a2075d404f52797f3e30b958a265
Lu <prlu1014@hs-karlsruhe.de>
2018-11-06 15:17:54 +0100

    Tasks can be created in the task view. Just refreshing ui wihtout updating the backend

--- swalab-frontend/src/main/java/com/swalab/frontend/gui/TaskPaneContent.java
+++ swalab-frontend/src/main/java/com/swalab/frontend/gui/TaskPaneContent.java
@@ -1,5 +1,6 @@
 package com.swalab.frontend.gui;
 
+import com.swalab.frontend.model.AbstractTaskAndNote;
 import com.swalab.frontend.model.Status;
 import com.swalab.frontend.model.Task;
 import com.swalab.frontend.model.Technican;
@@ -7,72 +8,169 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-public class TaskPaneContent extends AbstractPaneContent<Task> {
+public class TaskPaneContent extends AbstractPaneContent<AbstractTaskAndNote> {
 
     private Label _nameLabel;
-    private Label _descriptionlabel;
+    private Label _descriptionLabel;
     private Label _statusLabel;
-    private Label _creationLabell;
-    private Label _technicanLabel;
+    private Label _creationLabel;
+    private Label _technicianLabel;
+    private TextField _nameField;
+    private TextField _descriptionField;
+    private TextField _statusField;
+    private TextField _creationField;
+    private TextField _technicianField;
+    private Button _creationButton;
+    private ListView<AbstractTaskAndNote> _listView;
+    private Button _aboardButton;
 
     @Override
     public Parent getMainWindowContent() {
-        ObservableList<Task> list = FXCollections.observableArrayList();
+        ObservableList<AbstractTaskAndNote> list = FXCollections.observableArrayList();
         list.add(new Task("title", "description", null, null, null));
         list.add(new Task("title2", "description", null, null, null));
 
         BorderPane pane = new BorderPane();
         pane.setPrefWidth(200);
         pane.setBorder(createBorder());
-        ListView<Task> listView = createListView();
-        listView.setItems(list);
+        _listView = createListView();
+        _listView.setItems(list);
 
-        pane.setCenter(listView);
+        pane.setCenter(_listView);
+
+        HBox modificationBox = new HBox();
+        modificationBox.setSpacing(5);
+        modificationBox.setPadding(new Insets(3, 3, 3, 3));
+        Button taskCreationButton = new Button("+ Task");
+        taskCreationButton.setOnAction(ae -> openEditorView());
+        Button nodeCreationButton = new Button("+ Node");
+
+        modificationBox.getChildren().add(taskCreationButton);
+        modificationBox.getChildren().add(nodeCreationButton);
+
+        pane.setBottom(modificationBox);
 
         return pane;
+    }
+
+    private void openEditorView() {
+        setEditorMode(true);
+        _listView.getSelectionModel().select(null);
+        _nameField.requestFocus();
     }
 
     @Override
     public Parent getDescriptionWindowContent() {
-        GridPane pane=new GridPane();
-        pane.setPadding(new Insets(5,5,5,5));
-        pane.setVgap(3);
-        pane.setHgap(3);
-        Label nameLabel=new Label("Name");
-        Label descriptionLabel=new Label("Description");
-        Label statusLabel=new Label("Status");
-        Label creationLabel=new Label("Creation date");
-        Label technicanLabel=new Label("Technican");
+        GridPane descriptionPane = new GridPane();
+        descriptionPane.setPadding(new Insets(5, 5, 5, 5));
+        descriptionPane.setVgap(3);
+        descriptionPane.setHgap(3);
+        Label nameLabel = new Label("Name");
+        Label descriptionLabel = new Label("Description");
+        Label statusLabel = new Label("Status");
+        Label creationLabel = new Label("Creation date");
+        Label technicanLabel = new Label("Technican");
 
-        _nameLabel=new Label();
-        _descriptionlabel=new Label();
-        _statusLabel=new Label();
-        _creationLabell=new Label();
-        _technicanLabel=new Label();
+        _nameLabel = new Label();
+        _descriptionLabel = new Label();
+        _statusLabel = new Label();
+        _creationLabel = new Label();
+        _technicianLabel = new Label();
 
-        pane.addColumn(0,nameLabel,descriptionLabel,statusLabel,creationLabel,technicanLabel);
-        pane.addColumn(1,_nameLabel,_descriptionlabel,_statusLabel,_creationLabell,_technicanLabel);
+        _nameField = new TextField();
+        _descriptionField = new TextField();
+        _statusField = new TextField();
+        _creationField = new TextField();
+        _technicianField = new TextField("Change into combo box");
 
-        return pane;
+        _creationField.setDisable(true);
+        _creationField.setText(getCurrentTime());
+        _technicianField.setDisable(true);
+
+        _creationButton = new Button("Create");
+        // TODO check: Is task = node?
+        // TODO create object direct and send it to the server
+        _creationButton.setOnAction(ae -> {
+            Task task = new Task(_nameField.getText(), _descriptionField.getText(), null, null, null);
+            setEditorMode(false);
+            // temporary adding since ui doesn't have a connection to the backend
+            _listView.getItems().add(task);
+        });
+
+        _aboardButton = new Button("Abort");
+        _aboardButton.setOnAction(ae -> setEditorMode(false));
+
+        HBox creationButtonBox = new HBox();
+        creationButtonBox.getChildren().addAll(_creationButton, _aboardButton);
+        creationButtonBox.setPadding(new Insets(5, 5, 5, 5));
+        creationButtonBox.setSpacing(3);
+
+        setEditorMode(false);
+
+        descriptionPane.addColumn(0, nameLabel, descriptionLabel, statusLabel, creationLabel, technicanLabel);
+        descriptionPane.addColumn(1, _nameLabel, _descriptionLabel, _statusLabel, _creationLabel, _technicianLabel);
+        descriptionPane.addColumn(2, _nameField, _descriptionField, _statusField, _creationField, _technicianField, creationButtonBox);
+
+        return descriptionPane;
+    }
+
+    private void setEditorMode(boolean isEditorActive) {
+        _nameField.setVisible(isEditorActive);
+        _descriptionField.setVisible(isEditorActive);
+        _statusField.setVisible(isEditorActive);
+        _creationField.setVisible(isEditorActive);
+        _technicianField.setVisible(isEditorActive);
+        _creationButton.setVisible(isEditorActive);
+        _aboardButton.setVisible(isEditorActive);
+
+        _nameLabel.setVisible(!isEditorActive);
+        _descriptionLabel.setVisible(!isEditorActive);
+        _statusLabel.setVisible(!isEditorActive);
+        _creationLabel.setVisible(!isEditorActive);
+        _technicianLabel.setVisible(!isEditorActive);
+    }
+
+    private String getCurrentTime() {
+        StringBuilder builder = new StringBuilder();
+        Calendar calendar = GregorianCalendar.getInstance();
+        builder.append(calendar.get(Calendar.DATE));
+        builder.append('.');
+        builder.append(calendar.get(Calendar.MONTH) + 1);
+        builder.append('.');
+        builder.append(calendar.get(Calendar.YEAR));
+        return builder.toString();
     }
 
     @Override
-    protected void updateDescriptionContent(Task item) {
-        _nameLabel.setText(item.getName());
-        _descriptionlabel.setText(item.getDescription());
-        Status status = item.getStatus();
-        _statusLabel.setText(status==null?null:status.name());
-        Date creationDate = item.getCreationDate();
-        _creationLabell.setText(creationDate==null?null:creationDate.toGMTString());
-        Technican technican = item.getTechnican();
-        _technicanLabel.setText(technican==null?null:technican.getName());
+    protected void updateDescriptionContent(AbstractTaskAndNote item) {
+        if (item == null) {
+            _nameLabel.setText(null);
+            _descriptionLabel.setText(null);
+            _statusLabel.setText(null);
+            _creationLabel.setText(null);
+            _technicianLabel.setText(null);
+        } else {
+            _nameLabel.setText(item.getName());
+            _descriptionLabel.setText(item.getDescription());
+            Status status = item.getStatus();
+            _statusLabel.setText(status == null ? null : status.name());
+            Date creationDate = item.getCreationDate();
+            _creationLabel.setText(creationDate == null ? null : creationDate.toGMTString());
+            Technican technican = item.getTechnican();
+            _technicianLabel.setText(technican == null ? null : technican.getName());
+        }
     }
 
 
